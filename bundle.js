d5c1b772ea15d53d264fc3b0d14e5e5203ff9bdf
ImmDz <johnbessmerntyjj@gmail.com>
2023-10-27 10:40:19 +0300

    Updates

--- bundle.js
+++ bundle.js
@@ -1,1 +1,1 @@
-document.getElementById("fetch").addEventListener("click",(()=>{fetch("https://immdz.github.io/itprofit-test:9090/api/ping")}));
+document.getElementById("fetch").addEventListener("click",(async()=>{await fetch("http://localhost:9090/api/ping")}));
